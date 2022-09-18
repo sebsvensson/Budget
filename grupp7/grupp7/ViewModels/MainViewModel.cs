923bb8cb0500aca345a019bfba920a5f78af23fe
wictor arvidsson <wictor.arvidsson@hotmail.com>
2022-09-18 17:21:29 +0200

    mainview added

---
+++ grupp7/grupp7/ViewModels/MainViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace grupp7.ViewModels
+{
+    class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel _selectedViewModel;
+        public BaseViewModel SelectedViewModel
+        {
+            get { return _selectedViewModel; }
+            set
+            {
+                _selectedViewModel = value;
+                OnPropertyChanged(nameof(SelectedViewModel));
+            }
+        }
+
+        public MainViewModel()
+        {
+            //Set SelectedViewModel to startup UserControl here (Login view probably)
+        }
+    }
+}
