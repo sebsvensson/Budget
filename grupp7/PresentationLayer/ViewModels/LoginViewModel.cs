8456533f78b91d0395e4e25d0b20b1f0abd8796b
wictor arvidsson <wictor.arvidsson@hotmail.com>
2022-09-28 21:16:45 +0200

    fixed menu

--- grupp7/PresentationLayer/ViewModels/LoginViewModel.cs
+++ grupp7/PresentationLayer/ViewModels/LoginViewModel.cs
@@ -3,15 +3,36 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using PresentationLayer.Commands;
 using PresentationLayer.ViewModels;
+
 
 namespace PresentationLayer.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
+        private MainViewModel mainViewModel;
+        private ICommand _loginCommand;
+        public ICommand LogInCommand
+        {
+            get
+            {
+                return _loginCommand ?? (_loginCommand = new CommandHandler(() => LogIn()));
+            }
+        }
+
+        public LoginViewModel(MainViewModel mainViewModel)
+        {
+            this.mainViewModel = mainViewModel;
+        }
         private void LogIn()
         {
-            //mainViewModel.SelectedViewModel = new TestViewModel();
+            mainViewModel.SelectedViewModel = new TestViewModel();
+            mainViewModel.ColumnSpan = 2;
+            mainViewModel.GridColumn = 2;
+            mainViewModel.GridRow = 2;
+
         }
     }
 }
--- grupp7/PresentationLayer/ViewModels/MainViewModel.cs
+++ grupp7/PresentationLayer/ViewModels/MainViewModel.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using PresentationLayer.Commands;
 using PresentationLayer.ViewModels;
 
 namespace PresentationLayer.ViewModels
 {
-    class MainViewModel : BaseViewModel
+    public class MainViewModel : BaseViewModel
     {
         private BaseViewModel _selectedViewModel;
         public BaseViewModel SelectedViewModel
@@ -20,11 +22,80 @@ namespace PresentationLayer.ViewModels
             }
         }
 
+        private int _columnSpan = 2;
+        public int ColumnSpan
+        {
+            get { return _columnSpan; }
+            set
+            {
+                _columnSpan = value;
+                OnPropertyChanged(null);
+            }
+        }
+
+        private int _gridRow = 0;
+        public int GridRow
+        {
+            get { return _gridRow; }
+            set
+            {
+                _gridRow = value;
+                OnPropertyChanged(null);
+            }
+        }
+
+        private int _gridColumn = 0;
+        public int GridColumn
+        {
+            get { return _gridColumn; }
+            set
+            {
+                _gridColumn = value;
+                OnPropertyChanged(null);
+            }
+        }
+
+        private int _menuWidth = 150;
+        public int MenuWidth
+        {
+            get { return _menuWidth; }
+            set
+            {
+                _menuWidth = value;
+                OnPropertyChanged(null);
+            }
+        }
+
+        private bool menuWide = true;
+
+        private ICommand _menuDropCommand;
+        public ICommand MenuDropCommand
+        {
+            get
+            {
+                return _menuDropCommand ?? (_menuDropCommand = new CommandHandler(() => MenuDrop()));
+            }
+        }
+
+        private void MenuDrop()
+        {
+            if (menuWide)
+            {
+                MenuWidth = 15;
+                menuWide = false;
+            }
+            else
+            {
+                MenuWidth = 150;
+                menuWide = true;
+            }
+        }
+
         public MainViewModel()
         {
             //Set SelectedViewModel to startup UserControl here (Login view probably)
-            SelectedViewModel = new TestViewModel();
-            //SelectedViewModel = new LoginViewModel();
+            //SelectedViewModel = new TestViewModel();
+            SelectedViewModel = new LoginViewModel(this);
         }
     }
 }
