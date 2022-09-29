37aadf494cfe8145ff27f3a485338f44be0418f6
wictor arvidsson <wictor.arvidsson@hotmail.com>
2022-09-29 11:36:50 +0200

    added sub menus

--- grupp7/PresentationLayer/ViewModels/MainViewModel.cs
+++ grupp7/PresentationLayer/ViewModels/MainViewModel.cs
@@ -74,6 +74,72 @@ namespace PresentationLayer.ViewModels
             }
         }
 
+        //SUB MENU HEIGHTS
+
+        //budget
+        private bool _isCheckedBudget;
+        public bool IsCheckedBudget
+        {
+            get { return _isCheckedBudget; }
+            set
+            {
+                _isCheckedBudget = value;
+                OnPropertyChanged(null);
+
+                if (value == false)
+                {
+                    BudgetMenuHeight = 0;
+                }
+                else
+                {
+                    BudgetMenuHeight = 40;
+                }
+            }
+        }
+
+        private int _budgetMenuHeight = 0;
+        public int BudgetMenuHeight
+        {
+            get { return _budgetMenuHeight; }
+            set
+            {
+                _budgetMenuHeight = value;
+                OnPropertyChanged(null);
+            }
+        }
+
+        //product
+        private bool _isCheckedProduct;
+        public bool IsCheckedProduct
+        {
+            get { return _isCheckedProduct; }
+            set
+            {
+                _isCheckedProduct = value;
+                OnPropertyChanged(null);
+
+                if (value == false)
+                {
+                    ProductMenuHeight = 0;
+                }
+                else
+                {
+                    ProductMenuHeight = 40;
+                }
+            }
+        }
+
+        private int _ProductMenuHeight = 0;
+        public int ProductMenuHeight
+        {
+            get { return _ProductMenuHeight; }
+            set
+            {
+                _ProductMenuHeight = value;
+                OnPropertyChanged(null);
+            }
+        }
+
         //if menu is minimized or not
         private bool menuWide = true;
 
