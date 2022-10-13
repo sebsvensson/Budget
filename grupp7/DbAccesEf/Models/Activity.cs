82f5afc6076ae2864104acd31d291b8a685df12b
S204511 <s204511@student.hb.se>
2022-10-13 11:48:24 +0000

    Register activity view model

---
+++ grupp7/DbAccesEf/Models/Activity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DbAccesEf.Models
+{
+    public class Activity
+    {
+        public int ActivityID { get; set; }
+        public int CustomID { get; set; }
+        public string ActivityXxxx { get; set; }
+        public string ActivityName { get; set; }
+        public string AFFODepartment { get; set; }
+
+    }
+}
--- grupp7/PresentationLayer/ViewModels/RegisterActivityViewModel.cs
+++ grupp7/PresentationLayer/ViewModels/RegisterActivityViewModel.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BusinessLogic.Controllers;
 
 namespace PresentationLayer.ViewModels
 {
     public class RegisterActivityViewModel : BaseViewModel
     {
+        private ActivityController ActivityController;
+        private DbAccesEf.MyContext context;
+
+        //Bindings
+
     }
 }
